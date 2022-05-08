ca63455cc3fd0fa2e3b91fee6873137b002b9869
bRuttaZz <agrajpdasprivate@gmail.com>
2022-05-08 16:36:34 +0530

    on console

--- liveFace.js
+++ liveFace.js
@@ -8,10 +8,16 @@ const neeh = document.getElementById("neeh")
 // loading audio element
 // var beep = new Audio("/static/images/beep.mp3");
 
+const Neeh = document.getElementById("neeh")
+var faceMatcher = null
+
+const uri = "https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights/"
+const source = "https://raw.githubusercontent.com/bRuttaZz/NeehOnly/main/"
+// load model
 
 
 // load webcam video
-startVideo = async ()=>{
+startVideo = ()=>{
   console.log("models loaded");
 
   
@@ -26,56 +32,73 @@ startVideo = async ()=>{
   });
 }
 
-// repeated checking when video start capturing
-video.addEventListener('canplay',  ()=>{
-  // beep.play();
 
-  const canvas =  faceapi.createCanvasFromMedia(video)
-  document.getElementById("contentarea").append(canvas)
-  const outputSize = { width : video.width, height : video.height}
-  faceapi.matchDimensions(canvas, outputSize)
+loadModel = async ()=>{
+    // get inference 
+    console.log("models loaded");
 
-  var intervel = setInterval(async ()=>{
-      // detetcting number of faces and expressions of it
-      const heads = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions()
+    // starting video
+    startVideo();
 
-      // detect face features
-      const detection = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptor()
-
-      if (!detection){
-        return 0;
-      }
-
-      const resizedDetections = faceapi.resizeResults(detection, outputSize)
-      canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height)
-
-      // faceapi.draw.drawDetections(canvas, resizedDetections)
-      
-      var ctx = canvas.getContext('2d')
-      ctx.beginPath();
-      ctx.lineWidth = "3";
-      ctx.strokeStyle = "green";
-      ctx.rect(resizedDetections.alignedRect._box._x, resizedDetections.alignedRect._box._y, resizedDetections.alignedRect._box._width, resizedDetections.alignedRect._box._height);
-      ctx.stroke();
-      
-
-  }, 100);
-
-});
+    const results = await faceapi.detectSingleFace(neeh, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptor();
+    faceMatcher = new faceapi.FaceMatcher(results);
 
 
-// load model and start all processes
-// start execution
-function checkFace(){
-  let uri = "https://raw.githubusercontent.com/justadudewhohacks/face-api.js/master/weights/"
-  // load model
-  Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri(uri),
-    faceapi.nets.faceLandmark68Net.loadFromUri(uri),
-    faceapi.nets.faceRecognitionNet.loadFromUri(uri),
-    faceapi.nets.faceExpressionNet.loadFromUri(uri),
-  ]).then(startVideo).catch("Model is not loaded yet")
-};
+    // repeated checking when video start capturing
+    video.addEventListener('canplay',  ()=>{
+      // beep.play();
 
-checkFace();
+      const canvas =  faceapi.createCanvasFromMedia(video)
+      document.getElementById("contentarea").append(canvas)
+      const outputSize = { width : video.width, height : video.height}
+      faceapi.matchDimensions(canvas, outputSize)
+
+      setInterval(async ()=>{
+          // detetcting number of faces and expressions of it
+          const heads = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceExpressions()
+
+          // detect face features
+          const detection = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptor()
+
+          if (!detection){
+            return 0;
+          }
+
+          const resizedDetections = faceapi.resizeResults(detection, outputSize)
+          canvas.getContext('2d').clearRect(0,0, canvas.width, canvas.height)
+
+          const bestMatch = faceMatcher.findBestMatch(detection.descriptor)
+          console.log(bestMatch.toString())
+
+          // draw rectangle
+          // faceapi.draw.drawDetections(canvas, resizedDetections)
+          var ctx = canvas.getContext('2d')
+          ctx.beginPath();
+          ctx.lineWidth = "3";
+          ctx.strokeStyle = "green";
+          ctx.rect(resizedDetections.alignedRect._box._x, resizedDetections.alignedRect._box._y, resizedDetections.alignedRect._box._width, resizedDetections.alignedRect._box._height);
+          ctx.stroke();
+          
+
+      }, 100);
+
+    });
+
+
+  }
+
+
+
+
+
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri(uri),
+  faceapi.nets.faceLandmark68Net.loadFromUri(uri),
+  faceapi.nets.faceRecognitionNet.loadFromUri(uri),
+  faceapi.nets.faceExpressionNet.loadFromUri(uri),
+]).then(loadModel).catch("Model is not loaded yet");
+
+
+
 
