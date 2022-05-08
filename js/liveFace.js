f0fce8cdfaefc33af5986d61fb23f89ad2885fb2
bRuttaZz <agrajpdasprivate@gmail.com>
2022-05-08 22:33:38 +0530

    neeh

--- js/liveFace.js
+++ js/liveFace.js
@@ -1,7 +1,11 @@
 const video = document.getElementById("video");
-var numPeople = 1
-var happyIndex = 0
+var Neeh3d = 0
 var faceMatcher = null
+var not_neh = 0
+var intervel = null
+
+const MESSAGES = ["Nooo! Are you Neeh! you Sure??", "Where is Neeh I Cant See her", "Show me your face Neeh. Only Youu!", "Maahn Neeh Where are you?"]
+const NOMESSAGES = ["Mahn who are you? Where is Neeh", "No you're not Neeh"]
 
 const neeh = document.getElementById("neeh")
 const neeh2 = document.getElementById("neeh2")
@@ -31,6 +35,41 @@ startVideo = ()=>{
     modal.style.display = "block";
     
   });
+}
+
+// face dimentions
+function checkNoseToJawLength(landmarks){
+  if (landmarks){
+    const nose = landmarks.getNose()
+    const jawline = landmarks.getJawOutline()
+
+    topNose = nose[0]._x
+    
+    rightGap =  Math.sqrt( Math.pow((nose[0]._x - jawline[0]._x ),2) + Math.pow(( nose[0]._y - jawline[0]._y),2) );
+    width =  Math.sqrt( Math.pow((jawline[0]._x - jawline[15]._x ),2) + Math.pow(( jawline[0]._y - jawline[15]._y),2) );
+    
+    return rightGap/width
+  }
+  else{
+    return 0
+  }
+}
+
+
+
+function updateNeeh(landmarks){
+  // console.log(landmarks);
+  r = checkNoseToJawLength(landmarks)
+  console.log(r);
+  if (Neeh3d !=0 && (Neeh3d>r+0.1 || Neeh3d<r-0.1)){
+    return 1
+  }
+  else if(Neeh3d==0){
+    Neeh3d = r
+    document.getElementById("lookInstruction").innerHTML="Well you look a lot like her, can you just tilt your face a little bit for me to confirm.<br> please..."
+    return 2
+  }
+  return 3
 }
 
 
@@ -63,6 +102,10 @@ loadModel = async ()=>{
     // repeated checking when video start capturing
     video.addEventListener('canplay',  ()=>{
 
+      var intervel0 = setInterval(()=>{
+        document.getElementById("tension").innerHTML = MESSAGES[Math.floor(Math.random()*MESSAGES.length)];
+      }, 7000)
+
       var intervel = setInterval(async ()=>{
           // detect face features
           const detection = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptor()
@@ -72,17 +115,42 @@ loadModel = async ()=>{
           }
 
           if (faceMatcher.findBestMatch(detection.descriptor)._label == "Neeh"){
-  
-            document.getElementById("NeehMessage").style.display="block";
-            document.getElementById("contentarea").style.display = "none";
-            stopIntrvl();
+            var res = updateNeeh(detection.landmarks)
+            if ( res == 1){
+              document.getElementById("NeehMessage").style.display="block";
+              document.getElementById("contentarea").style.display = "none";
+              document.getElementById("LookForHer").style.display = "none";
+              stopChange();
+              stopIntrvl();
+            }
+            else if (res == 2){
+              stopChange();
+            }
           }
           else{
-
+            if(not_neh===0){
+              stopChange();
+              document.getElementById("tension").innerHTML = "Noo! Noo! Noo! It's not Neeh";
+              intervel1 = setInterval(()=>{
+                document.getElementById("tension").innerHTML = NOMESSAGES[Math.floor(Math.random()*NOMESSAGES.length)];
+              },7000);
+              not_neh++;
+            }
+            
           }      
           delete detection;      
       }, 250);
 
+    function stopChange(){
+      try{
+        clearInterval(intervel0);
+        clearInterval(intervel1) ; 
+      }
+      catch(e){
+        console.log("Okay not set");
+      }
+      document.getElementById("tension").innerHTML = "";   
+    }
     // stoping the intervel if needed
     function stopIntrvl(){
       clearInterval(intervel);
